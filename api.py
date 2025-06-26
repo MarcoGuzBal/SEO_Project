d30259af7e7b8b9d43f3c9907a9483ecff7177dc
Marco Guzman-Balcazar <marcoaguzmanbalcazar@gmail.com>
2025-06-26 17:37:51 +0000

    Turned api data into database

--- api.py
+++ api.py
@@ -1,10 +1,19 @@
 import requests
 import os
+import pandas as pd
+import sqlalchemy as db
 
 API_KEY=os.environ.get('EXCHANGERATE_API_KEY')
 
 url=f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD"
 
-response = requests.get(url)
+response = requests.get(url).json()
+engine = db.create_engine('sqlite:///data_base_name.db')
 
-print(response.json())
+dataframe_name=pd.DataFrame.from_dict(response)
+
+dataframe_name.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
